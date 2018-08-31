e3fafad8948c4059a7f1df39952550ac8ae92bec
Jamie <jamie.adams@tensix.com>
2018-08-31 04:12:21 +0100

    First proper commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const path = require("path");
-//Test7
+const config = require("./config.json");
+if (config.database == "sqlite3") {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(config.dir);
+} else {
+    console.error("Database not supported at this time, sorry.")
+    process.exit();
+}
